5911252639199c97ffe5f2d02130c7041fed7826
jmctune <17505625+jmctune@users.noreply.github.com>
2021-12-09 20:10:08 -0600

    check for english before translating and probably other stuff

--- app/api_translate/cutscene.py
+++ app/api_translate/cutscene.py
@@ -39,9 +39,14 @@ try:
         write_bytes,
         read_string,
         scan_backwards)
-    from errors import AddressOutOfRange
-    from translate import sanitized_dialog_translate, sqlite_read, sqlite_write_dynamic
+    from translate import (
+        sanitized_dialog_translate,
+        sqlite_read,
+        sqlite_write,
+        detect_lang
+    )
     from signatures import index_pattern, foot_pattern
+    from errors import AddressOutOfRange
 
     def setup_logger(name, log_file, level=logging.INFO):
         formatter = logging.Formatter('%(asctime)s %(message)s')
@@ -64,24 +69,27 @@ try:
 
     # get address values where text can be identified
     ja_address = unpack_to_int({esi_address})[0]
-    logger.debug('cutscene address found :: checking if translation is needed')
     ja_text = read_string(ja_address)
-    logger.debug(ja_text)
-    result = sqlite_read(ja_text, 'en')
-    
-    if api_logging:
-        game_text_logger.info(ja_text)
 
-    if result is not None:
-        logger.debug('found database entry. no translation needed')
-        write_bytes(ja_address, result.encode() + b'\x00')
+    if detect_lang(ja_text):
+        logger.debug('cutscene text found :: checking if translation is needed')
+        result = sqlite_read(ja_text, '{api_region}', 'dialog')
+
+        if api_logging:
+            game_text_logger.info(ja_text)
+
+        if result is not None:
+            logger.debug('found database entry. no translation needed')
+            write_bytes(ja_address, result.encode() + b'\x00')
+        else:
+            logger.debug('translation needed. sending to {api_service}')
+            translated_text = sanitized_dialog_translate('{api_service}', '{api_pro}', ja_text, '{api_key}', '{api_region}')
+            logger.debug(translated_text)
+            sqlite_write(ja_text, 'dialog', translated_text, '{api_region}')
+            logger.debug('database record inserted.')
+            write_bytes(ja_address, translated_text.encode() + b'\x00')
     else:
-        logger.debug('translation needed. sending to {api_service}')
-        translated_text = sanitized_dialog_translate('{api_service}', '{api_pro}', ja_text, '{api_key}', '{api_region}')
-        logger.debug(translated_text)
-        sqlite_write_dynamic(ja_text, '', translated_text, 'en')
-        logger.debug('database record inserted.')
-        write_bytes(ja_address, translated_text.encode() + b'\x00')
+        logger.debug('English detected. Doing nothing.')
 except AddressOutOfRange:
     pass
 except:
@@ -92,12 +100,7 @@ except:
     return str(shellcode)
 
 def cutscene_file_dump_shellcode(
-    edi_address: str,
-    api_service: str,
-    api_key: str,
-    api_pro: str,
-    api_logging: str,
-    api_region: str) -> str:
+    edi_address: str) -> str:
     '''
     Returns shellcode for the cutscene file dump function hook.
 
@@ -149,17 +152,20 @@ try:
 
     # get address values where text can be identified
     ja_address = unpack_to_int({edi_address})[0]
-
     logger.debug('adhoc cutscene file found :: checking if we have this file')
     adhoc_address = scan_backwards(ja_address, index_pattern)
-    adhoc_bytes = read_bytes(adhoc_address, 64)
-    adhoc_write = write_adhoc_entry(adhoc_address, str(adhoc_bytes.hex()))
-    if adhoc_write['success']:
-        logger.debug('Wrote adhoc file.')
-    elif adhoc_write['file'] is not None:
-        logger.debug('New cutscene adhoc file. Will write to new_adhoc_dumps if it does not already exist.')
-    elif adhoc_write['file'] is None:
-        logger.debug('This file already exists in new_adhoc_dumps. Needs merge into github.')
+    if read_bytes(adhoc_address - 2, 1) != b'\x69':
+        adhoc_bytes = read_bytes(adhoc_address, 64)
+        adhoc_write = write_adhoc_entry(adhoc_address, str(adhoc_bytes.hex()))
+        if adhoc_write['success']:
+            logger.debug('Wrote adhoc file.')
+        elif adhoc_write['file'] is not None:
+            logger.debug('New cutscene adhoc file. Will write to new_adhoc_dumps if it does not already exist.')
+        elif adhoc_write['file'] is None:
+            logger.debug('This file already exists in new_adhoc_dumps. Needs merge into github.')
+        write_bytes(adhoc_address - 2, b'\x69')  # write our state byte so we know we already wrote this. nice.
+    else:
+        logger.debug('We already wrote this cutscene file. Ignoring.')
 except:
     with open('out.log', 'a+') as f:
         f.write(format_exc())
