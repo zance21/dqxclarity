79880880dc7558b96422554b18a1559d87dbf5c5
jmctune <17505625+jmctune@users.noreply.github.com>
2021-12-31 22:39:16 -0600

    This hook hits two times. First has eax under 0x15000000, second is way higher. Only run this once.

--- app/api_translate/quest.py
+++ app/api_translate/quest.py
@@ -3,8 +3,7 @@ import os
 from json import dumps
 
 def quest_text_shellcode(
-    ebx_address: int,
-    esi_address: int,
+    eax_address: int,
     api_service: str,
     api_key: str,
     api_pro: str,
@@ -14,7 +13,6 @@ def quest_text_shellcode(
     '''
     Returns shellcode for the translate function hook.
     eax_address: Where text can be modified to be fed to the screen
-    ebx_address: NPC name
     '''
     local_paths = dumps(sys.path).replace('\\', '\\\\')
     working_dir = dumps(os.getcwd()).replace('\\', '\\\\')
@@ -67,47 +65,47 @@ try:
 
     quest_file = 'adhoc_quests_requests'
 
-    quest_names_addr = unpack_to_int({esi_address})[0]
-    quest_desc_addr = unpack_to_int({ebx_address})[0]
+    quest_addr = unpack_to_int({eax_address})[0]
 
-    subquest_name_addr = quest_names_addr + 20
-    quest_name_addr = quest_names_addr + 76
-    quest_desc_addr = quest_desc_addr + 132
-    quest_rewards_addr = quest_desc_addr + 508
-    quest_repeat_rewards_addr = quest_desc_addr + 612
+    if quest_addr < 0x15000000:
+        subquest_name_addr = quest_addr + 20
+        quest_name_addr = quest_addr + 76
+        quest_desc_addr = quest_addr + 132
+        quest_rewards_addr = quest_addr + 640
+        quest_repeat_rewards_addr = quest_addr + 744
 
-    subquest_name_ja = read_string(subquest_name_addr)
-    logger.debug('Subquest: ' + str(subquest_name_ja))
-    quest_name_ja = read_string(quest_name_addr)
-    logger.debug('Quest Name: ' + str(quest_name_ja))
-    quest_desc_ja = read_string(quest_desc_addr)
-    logger.debug('Quest Description: ' + str(quest_desc_ja))
-    quest_rewards_ja = read_string(quest_rewards_addr)
-    logger.debug('Quest Rewards: ' + str(quest_rewards_ja))
-    quest_repeat_rewards_ja = read_string(quest_repeat_rewards_addr)
-    logger.debug('Quest Repeat Rewards: ' + str(quest_repeat_rewards_ja))
+        subquest_name_ja = read_string(subquest_name_addr)
+        logger.debug('Subquest: ' + str(subquest_name_ja))
+        quest_name_ja = read_string(quest_name_addr)
+        logger.debug('Quest Name: ' + str(quest_name_ja))
+        quest_desc_ja = read_string(quest_desc_addr)
+        logger.debug('Quest Description: ' + str(quest_desc_ja))
+        quest_rewards_ja = read_string(quest_rewards_addr)
+        logger.debug('Quest Rewards: ' + str(quest_rewards_ja))
+        quest_repeat_rewards_ja = read_string(quest_repeat_rewards_addr)
+        logger.debug('Quest Repeat Rewards: ' + str(quest_repeat_rewards_ja))
 
-    if detect_lang(quest_desc_ja):
-        if subquest_name_ja:
-            subquest_name_en = query_string_from_file(subquest_name_ja, quest_file)
-            if subquest_name_en:
-                write_bytes(subquest_name_addr, str.encode(subquest_name_en) + b'\x00')
-        if quest_name_ja:
-            quest_name_en = query_string_from_file(quest_name_ja, quest_file)
-            if quest_name_en:
-                write_bytes(quest_name_addr, str.encode(quest_name_en) + b'\x00')
-        if quest_rewards_ja:
-            quest_rewards_en = clean_up_and_return_items(quest_rewards_ja)
-            if quest_rewards_en:
-                write_bytes(quest_rewards_addr, str.encode(quest_rewards_en) + b'\x00')
-        if quest_repeat_rewards_ja:
-            quest_repeat_rewards_en = clean_up_and_return_items(quest_repeat_rewards_ja)
-            if quest_repeat_rewards_en:
-                write_bytes(quest_repeat_rewards_addr, str.encode(quest_repeat_rewards_en) + b'\x00')
-        if quest_desc_ja:
-            quest_desc_en = quest_translate('{api_service}', '{api_pro}', quest_desc_ja, '{api_key}', '{api_region}')
-            if quest_desc_en:
-                write_bytes(quest_desc_addr, str.encode(quest_desc_en) + b'\x00')
+        if detect_lang(quest_desc_ja):
+            if subquest_name_ja:
+                subquest_name_en = query_string_from_file(subquest_name_ja, quest_file)
+                if subquest_name_en:
+                    write_bytes(subquest_name_addr, str.encode(subquest_name_en) + b'\x00')
+            if quest_name_ja:
+                quest_name_en = query_string_from_file(quest_name_ja, quest_file)
+                if quest_name_en:
+                    write_bytes(quest_name_addr, str.encode(quest_name_en) + b'\x00')
+            if quest_rewards_ja:
+                quest_rewards_en = clean_up_and_return_items(quest_rewards_ja)
+                if quest_rewards_en:
+                    write_bytes(quest_rewards_addr, str.encode(quest_rewards_en) + b'\x00')
+            if quest_repeat_rewards_ja:
+                quest_repeat_rewards_en = clean_up_and_return_items(quest_repeat_rewards_ja)
+                if quest_repeat_rewards_en:
+                    write_bytes(quest_repeat_rewards_addr, str.encode(quest_repeat_rewards_en) + b'\x00')
+            if quest_desc_ja:
+                quest_desc_en = quest_translate('{api_service}', '{api_pro}', quest_desc_ja, '{api_key}', '{api_region}')
+                if quest_desc_en:
+                    write_bytes(quest_desc_addr, str.encode(quest_desc_en) + b'\x00')
 except:
     with open('out.log', 'a+') as f:
         f.write(format_exc())
