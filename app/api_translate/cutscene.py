a45244d7723e02dbe465d20fd2b651b02463e7c7
jmctune <17505625+jmctune@users.noreply.github.com>
2021-12-31 22:38:22 -0600

    Remove deepl translation shellcode. Relying on adhoc files for translation now due to issues.

--- app/api_translate/cutscene.py
+++ app/api_translate/cutscene.py
@@ -3,103 +3,6 @@ import os
 from json import dumps
 
 def cutscene_shellcode(
-    esi_address: str,
-    api_service: str,
-    api_key: str,
-    api_pro: str,
-    api_logging: str,
-    api_region: str,
-    debug: bool) -> str:
-    '''
-    Returns shellcode for the cutscene function hook.
-
-    esi_address: Where text for cutscene triggers can be found.
-    '''
-    local_paths = dumps(sys.path).replace('\\', '\\\\')
-    working_dir = dumps(os.getcwd()).replace('\\', '\\\\')
-
-    shellcode = fr"""
-import sys
-import os
-from traceback import format_exc
-from os import chdir
-
-local_paths = {local_paths}
-working_dir = {working_dir}
-debug = {debug}
-sys.path = local_paths
-chdir(working_dir)
-
-try:
-    import logging
-    from hook import unpack_to_int
-    from clarity import write_adhoc_entry
-    from memory import (
-        read_bytes,
-        write_bytes,
-        read_string,
-        scan_backwards)
-    from translate import (
-        sanitized_dialog_translate,
-        sqlite_read,
-        sqlite_write,
-        detect_lang
-    )
-    from signatures import index_pattern, foot_pattern
-    from errors import AddressOutOfRange
-
-    def setup_logger(name, log_file, level=logging.INFO):
-        formatter = logging.Formatter('%(asctime)s %(message)s')
-        handler = logging.FileHandler(log_file, encoding='utf-8')
-        handler.setFormatter(formatter)
-
-        logger = logging.getLogger(name)
-        if (logger.hasHandlers()):
-            logger.handlers.clear()
-
-        logger.setLevel(level)
-        logger.addHandler(handler)
-
-        return logger
-
-    logger = setup_logger('out', 'out.log')
-    if debug:
-        logger.setLevel(logging.DEBUG)
-    game_text_logger = setup_logger('gametext', 'game_text.log')
-
-    # get address values where text can be identified
-    ja_address = unpack_to_int({esi_address})[0]
-    ja_text = read_string(ja_address)
-
-    if detect_lang(ja_text):
-        logger.debug('cutscene text found :: checking if translation is needed')
-        result = sqlite_read(ja_text, '{api_region}', 'dialog')
-
-        if {api_logging}:
-            game_text_logger.info(ja_text)
-
-        if result is not None:
-            logger.debug('found database entry. no translation needed')
-            write_bytes(ja_address, result.encode() + b'\x00')
-        else:
-            logger.debug('translation needed. sending to {api_service}')
-            translated_text = sanitized_dialog_translate('{api_service}', '{api_pro}', ja_text, '{api_key}', '{api_region}')
-            logger.debug(translated_text)
-            sqlite_write(ja_text, 'dialog', translated_text, '{api_region}')
-            logger.debug('database record inserted.')
-            write_bytes(ja_address, translated_text.encode() + b'\x00')
-    else:
-        logger.debug('English detected. Doing nothing.')
-except AddressOutOfRange:
-    pass
-except:
-    with open('out.log', 'a+') as f:
-        f.write(format_exc())
-    """
-
-    return str(shellcode)
-
-def cutscene_file_dump_shellcode(
     edi_address: str) -> str:
     '''
     Returns shellcode for the cutscene file dump function hook.
@@ -169,4 +72,4 @@ except:
         f.write(format_exc())
     """
 
-    return shellcode
+    return str(shellcode)
