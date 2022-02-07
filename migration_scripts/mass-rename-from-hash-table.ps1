2f103c1e06c2a16fa463631a6c5bb20bd58c5b56
jmctune <17505625+jmctune@users.noreply.github.com>
2022-02-06 20:02:15 -0600

    changes

--- migration_scripts/mass-rename-from-hash-table.ps1
+++ migration_scripts/mass-rename-from-hash-table.ps1
@@ -1,11 +1,13 @@
 
 $hash = @{
-    "00000012.json" = "adhoc_select_options.json"
-    "00000056.json" = "adhoc_gold_lost_string.json"
+    "57.json" = "locations_housing.json"
+    "58.json" = "glamour.json"
 }
 
+$JsonPath = "..\json\_lang"
+
 ForEach ($h in $hash.GetEnumerator()) {
-    Move-Item -Path ".\json\_lang\en\$($h.Name)" -Destination ".\json\_lang\en\$($h.Value)"
-    Move-Item -Path ".\json\_lang\ja\$($h.Name)" -Destination ".\json\_lang\ja\$($h.Value)"
-    ((Get-Content -Path .\app\hex_dict.csv -Raw) -replace "json\\_lang\\en\\$($h.Name)", "json\_lang\en\$($h.Value)") | Set-Content -Path .\app\hex_dict.csv
+        Move-Item -Path $JsonPath\en\$($h.Name) -Destination $JsonPath\en\$($h.Value)
+        Move-Item -Path $JsonPath\ja\$($h.Name) -Destination $JsonPath\ja\$($h.Value)
+        ((Get-Content -Path ..\app\hex_dict.csv -Raw) -replace "json\\_lang\\en\\$($h.Name)", "json\_lang\en\$($h.Value)") | Set-Content -Path ..\app\hex_dict.csv
 }
